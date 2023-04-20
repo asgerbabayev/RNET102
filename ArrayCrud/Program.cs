6ced13cccb73901b3cd6014b09f0490a3ce21053
asgerbabayev <asgerbabayev@hotmail.com>
2023-04-20 15:59:37 +0400

    array sort without third parameter

---
+++ ArrayCrud/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ArrayCrud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort(new int[] { 1, 20, -7, 3, 54, 34, 5 });
+        }
+        public static void Sort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        numbers[i] = numbers[i] + numbers[j];
+                        numbers[j] = numbers[i] - numbers[j];
+                        numbers[i] = numbers[i] - numbers[j];
+                    }
+                }
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+
+
+    }
+}
